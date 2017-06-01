03f3b00e18d93589d624449f7e674ed605fb4408
wayerr <radiofun@ya.ru>
2017-06-01 14:44:50 +0300

    fix warn in ProcessorTest.kt

---
+++ core/src/test/java/com/codeabovelab/tpc/core/processor/ProcessorTest.kt
@@ -0,0 +1,21 @@
+package com.codeabovelab.tpc.core.processor
+
+import com.codeabovelab.tpc.doc.DocumentImpl
+import org.junit.Test
+
+/**
+ */
+class ProcessorTest {
+
+    @Test
+    fun simpleTest() {
+        val processor = Processor()
+        processor.addRule(Rule("simple", 0F, RegexPredicate("MARK"), SetAttributeAction("found", "mark")))
+        val firstReport = processor.process(DocumentImpl.Builder()
+          .id("first")
+          .body("some text MARK \n ant some other MARK text \n MARK")
+          .build())
+        System.out.println(firstReport)
+    }
+
+}
