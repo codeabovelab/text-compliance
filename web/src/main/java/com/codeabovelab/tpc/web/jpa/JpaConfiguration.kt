06c11864d76648204798c5225e24d15833163c45
wayerr <radiofun@ya.ru>
2017-07-13 18:57:26 +0300

    allow JpaConfiguration use file backed HSQL configuration

--- web/src/main/java/com/codeabovelab/tpc/web/jpa/JpaConfiguration.kt
+++ web/src/main/java/com/codeabovelab/tpc/web/jpa/JpaConfiguration.kt
@@ -1,16 +1,19 @@
 package com.codeabovelab.tpc.web.jpa
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.autoconfigure.domain.EntityScan
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import javax.sql.DataSource
 
 /**
  */
 @Import(
-        DataSourceAutoConfiguration::class,
         HibernateJpaAutoConfiguration::class
 )
 @EntityScan(
@@ -26,4 +29,17 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 @Configuration
 open class JpaConfiguration {
 
+
+    @EnableConfigurationProperties(DataSourceProperties::class)
+    @Configuration
+    open class DataSourceConfig(
+        var properties: DataSourceProperties
+    ) {
+
+        @Bean
+        open fun dataSource() : DataSource? {
+            val builder = properties.initializeDataSourceBuilder()
+            return builder.build()
+        }
+    }
 }
