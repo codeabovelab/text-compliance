369b93432390a040bb61d41b53d29583dda12718
wayerr <radiofun@ya.ru>
2017-07-18 18:41:18 +0300

    update jpa entities

--- web/src/main/java/com/codeabovelab/tpc/web/jpa/DocEntity.kt
+++ web/src/main/java/com/codeabovelab/tpc/web/jpa/DocEntity.kt
@@ -9,14 +9,14 @@ open class DocEntity {
     @Id
     @GeneratedValue
     var id: Long = 0
-    var type: String = ""
+    lateinit var type: String
     @Column(unique = true)
-    var documentId: String = ""
+    lateinit var documentId: String
     /**
      * Raw data of document
      */
     @Column(length = 10240)
-    var data: ByteArray = byteArrayOf()
+    lateinit var data: ByteArray
     var binary: Boolean = true
     @Column(length = 10240 * 4)
     var report: String? = null
--- web/src/main/java/com/codeabovelab/tpc/web/jpa/RulesRepository.kt
+++ web/src/main/java/com/codeabovelab/tpc/web/jpa/RulesRepository.kt
@@ -1,8 +1,10 @@
 package com.codeabovelab.tpc.web.jpa
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
 /**
  */
+@Repository
 interface RulesRepository: JpaRepository<RuleEntity, Long> {
 }
