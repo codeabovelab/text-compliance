cbfc7010ce8b359bc6a9dd7905571bdac2c685bf
wayerr <radiofun@ya.ru>
2017-07-18 19:43:44 +0300

    fix jpa entities

--- web/src/main/java/com/codeabovelab/tpc/web/jpa/DocEntity.kt
+++ web/src/main/java/com/codeabovelab/tpc/web/jpa/DocEntity.kt
@@ -9,13 +9,15 @@ open class DocEntity {
     @Id
     @GeneratedValue
     var id: Long = 0
+
+    @Column(nullable = false)
     lateinit var type: String
-    @Column(unique = true)
+    @Column(unique = true, nullable = false)
     lateinit var documentId: String
     /**
      * Raw data of document
      */
-    @Column(length = 10240)
+    @Column(length = 10240, nullable = false)
     lateinit var data: ByteArray
     var binary: Boolean = true
     @Column(length = 10240 * 4)
--- web/src/main/java/com/codeabovelab/tpc/web/jpa/RuleEntity.kt
+++ web/src/main/java/com/codeabovelab/tpc/web/jpa/RuleEntity.kt
@@ -12,11 +12,16 @@ open class RuleEntity {
     @Id
     @GeneratedValue
     var id: Long = 0
-    @Column(unique = true)
+
+    @Column(unique = true, nullable = false)
     lateinit var ruleId: String
+
+    @Column(nullable = false)
     var weight: Float = 1f
-    @Column(length = 10240)
+
+    @Column(length = 10240, nullable = false)
     lateinit var predicate: String
+
     @Column(length = 10240)
     var action: String? = null
 }
