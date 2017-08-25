368773ce4d6224a0a5fe8ab3a02670b226997e6f
wayerr <radiofun@ya.ru>
2017-08-25 16:37:03 +0300

    RegexPredicate now skip empty matches & enable DOTALL | MULTILINE flags

--- core/src/main/java/com/codeabovelab/tpc/core/processor/RegexPredicate.kt
+++ core/src/main/java/com/codeabovelab/tpc/core/processor/RegexPredicate.kt
@@ -16,15 +16,20 @@ class RegexPredicate(
         val pattern: String
 ): RulePredicate<PredicateResult<*>> {
 
-    private val compiled: Pattern = Pattern.compile(this.pattern)
+    private val compiled: Pattern = Pattern.compile(this.pattern, Pattern.MULTILINE or Pattern.DOTALL)
 
     override fun test(pc: PredicateContext, text: Text): PredicateResult<*> {
         val matcher = compiled.matcher(text.data)
         var list: MutableList<PredicateResult.Entry>? = null
         while(matcher.find()) {
-            val offset = matcher.start()
-            val len = matcher.end() - offset
-            val coord = text.getCoordinates(offset, len)
+            val start = matcher.start()
+            val end = matcher.end()
+            if(start == end) {
+                // it happens on some patterns
+                continue
+            }
+            val len = end - start
+            val coord = text.getCoordinates(start, len)
             if(list == null) {
                 list = ArrayList()
             }
