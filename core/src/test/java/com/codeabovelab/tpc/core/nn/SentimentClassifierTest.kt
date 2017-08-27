20d2091315e177ca2d34a29e00f363eb32189bc4
pronto <vitaly.pronto@gmail.com>
2017-08-27 20:01:50 +0300

    added test text

--- core/src/test/java/com/codeabovelab/tpc/core/nn/SentimentClassifierTest.kt
+++ core/src/test/java/com/codeabovelab/tpc/core/nn/SentimentClassifierTest.kt
@@ -19,7 +19,8 @@ class SentimentClassifierTest {
         val wordVectorFile = Paths.get("/home/pronto/Downloads/sentiment/vectors-negative300.bin/vectors.bin")
         val classifier = SentimentClassifier(modelFile = modelFile, wordVectorFile = wordVectorFile)
 
-        val result = classifier.test(PredicateContext.create(), TextImpl(""))
+        val result = classifier.test(PredicateContext.create(), TextImpl(
+                "This is a terrible movie, don't waste your money on it. Don't even watch it for free. That's all I have to say."))
         log.info("result {}", result)
         val labels = result.labels
         assertTrue(labels.isNotEmpty())
