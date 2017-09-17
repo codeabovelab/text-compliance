516719a89e41729bd36e2e493d4fd0e67ca3b335
pronto <vitaly.pronto@gmail.com>
2017-09-17 16:02:46 +0300

    disabled cuda

--- tool/src/main/java/com/codeabovelab/tpc/tool/learn/PerfomanceSettings.kt
+++ tool/src/main/java/com/codeabovelab/tpc/tool/learn/PerfomanceSettings.kt
@@ -1,6 +1,6 @@
 package com.codeabovelab.tpc.tool.learn
 
-import org.nd4j.jita.conf.CudaEnvironment
+//import org.nd4j.jita.conf.CudaEnvironment
 import org.nd4j.linalg.factory.Nd4j
 
 object PerfomanceSettings {
@@ -10,12 +10,12 @@ object PerfomanceSettings {
     }
 
     fun useCuda() {
-        CudaEnvironment.getInstance().getConfiguration() //https://deeplearning4j.org/gpu
-                .setMaximumGridSize(512)
-                .setMaximumBlockSize(512)
-                .setMaximumDeviceCacheableLength(1024 * 1024 * 1024L)
-                .setMaximumDeviceCache(6L * 1024 * 1024 * 1024L)
-                .setMaximumHostCacheableLength(1024 * 1024 * 1024L)
-                .setMaximumHostCache(6L * 1024 * 1024 * 1024L)
+//        CudaEnvironment.getInstance().getConfiguration() //https://deeplearning4j.org/gpu
+//                .setMaximumGridSize(512)
+//                .setMaximumBlockSize(512)
+//                .setMaximumDeviceCacheableLength(1024 * 1024 * 1024L)
+//                .setMaximumDeviceCache(6L * 1024 * 1024 * 1024L)
+//                .setMaximumHostCacheableLength(1024 * 1024 * 1024L)
+//                .setMaximumHostCache(6L * 1024 * 1024 * 1024L)
     }
 }
